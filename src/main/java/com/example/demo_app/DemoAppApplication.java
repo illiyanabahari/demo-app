45550f24fb3b8415fe4b9e4a541a6f9529c2ad2d
illiyanabahari <illiyanabahari@gmail.com>
2026-03-11 13:22:24 +0800

    Initial commit of demo-app

---
+++ src/main/java/com/example/demo_app/DemoAppApplication.java
@@ -0,0 +1,29 @@
+package com.example.demo_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@Controller
+public class DemoAppApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DemoAppApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoAppApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "index"; // serves src/main/resources/templates/index.html
+    }
+}
+
+
